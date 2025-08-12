2c1f78f0c82f3e62f79c452a498bfc9df389dc1f
Javier Cubero <102825817+JaviCubero@users.noreply.github.com>
2025-08-12 13:44:11 +0000

    Proyecto terminado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,99 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, String, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List, Optional
+from sqlalchemy.dialects.postgresql import JSONB
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+fav_characters = Table(
+    "fav_characters", db.Model.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+    Column("character_id", ForeignKey("character.id"), primary_key=True),
+)
 
+fav_vehicles = Table(
+    "fav_vehicles", db.Model.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+    Column("vehicle_id", ForeignKey("vehicle.id"), primary_key=True),
+)
+
+fav_planets = Table(
+    "fav_planets", db.Model.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+    Column("planet_id", ForeignKey("planet.id"), primary_key=True),
+)
+
+class User(db.Model):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), nullable=False)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    name: Mapped[Optional[str]] = mapped_column(String(50))
+    lastname: Mapped[Optional[str]] = mapped_column(String(50))
+
+    fav_characters: Mapped[List["Character"]] = relationship(secondary=fav_characters, back_populates="fav_by_users")
+    fav_vehicles: Mapped[List["Vehicle"]] = relationship(secondary=fav_vehicles, back_populates="fav_by_users")
+    fav_planets: Mapped[List["Planet"]] = relationship(secondary=fav_planets, back_populates="fav_by_users")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "lastname": self.lastname,
+            "fav_characters": [character.serialize() for character in self.fav_characters],
+            "fav_vehicles": [vehicle.serialize() for vehicle in self.fav_vehicles],
+            "fav_planets": [planet.serialize() for planet in self.fav_planets]
+        }
+
+class Character(db.Model):
+    __tablename__ = "character"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    image: Mapped[Optional[str]] = mapped_column(String(120))
+    attributes: Mapped[Optional[dict]] = mapped_column(JSONB)
+
+    fav_by_users: Mapped[List[User]] = relationship(secondary=fav_characters, back_populates="fav_characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "attributes": self.attributes
+        }
+
+class Vehicle(db.Model):
+    __tablename__ = "vehicle"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    image: Mapped[Optional[str]] = mapped_column(String(120))
+    attributes: Mapped[Optional[dict]] = mapped_column(JSONB)
+
+    fav_by_users: Mapped[List[User]] = relationship(secondary=fav_vehicles, back_populates="fav_vehicles")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "attributes": self.attributes
+        }
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    image: Mapped[Optional[str]] = mapped_column(String(120))
+    attributes: Mapped[Optional[dict]] = mapped_column(JSONB)
+
+    fav_by_users: Mapped[List[User]] = relationship(secondary=fav_planets, back_populates="fav_planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "attributes": self.attributes
         }
